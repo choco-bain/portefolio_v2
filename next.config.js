4560f087fb0899a21d82149931deb2b278e5c09f
choco-bain <chocobain225@gmail.com>
2026-05-05 18:57:58 +0200

    Fix: add basePath for portfolio_v2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  basePath: '/portfolio_v2',
+  basePath: '/portefolio_v2',
   reactStrictMode: true,
   images: {
     unoptimized: true
