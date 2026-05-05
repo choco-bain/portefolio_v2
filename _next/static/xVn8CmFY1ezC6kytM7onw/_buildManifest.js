0685587d03674a585b1fb8efdadd5635469b2915
choco-bain <chocobain225@gmail.com>
2026-05-05 19:17:15 +0200

    Updates

---
+++ _next/static/xVn8CmFY1ezC6kytM7onw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-a902d97551b42717.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
